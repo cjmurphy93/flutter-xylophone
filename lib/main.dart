9568ba68bb75312b4c2948c80b29f8b923d9baa8
cjmurphy93 <cjmurph93@gmail.com>
2021-05-27 00:44:21 -0400

    configured textbutton

--- lib/main.dart
+++ lib/main.dart
@@ -11,11 +11,24 @@ class XylophoneApp extends StatelessWidget {
         body: SafeArea(
           child: Center(
               child: TextButton(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(Colors.red),
+                  overlayColor: MaterialStateProperty.resolveWith<Color>(
+                        (Set<MaterialState> states) {
+                      if (states.contains(MaterialState.hovered))
+                        return Colors.grey.withOpacity(0.04);
+                      if (states.contains(MaterialState.focused) ||
+                          states.contains(MaterialState.pressed))
+                        return Colors.grey.withOpacity(0.12);
+                      return null; // Defer to the widget's default.
+                    },
+                  ),
+                ),
                   onPressed: () {
                     final player = AudioCache();
                     player.play('note1.wav');
                   },
-                  child: Text('Click Me'))),
+                  )),
         ),
       ),
     );
