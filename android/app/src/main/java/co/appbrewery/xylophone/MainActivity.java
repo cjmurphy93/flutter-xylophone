d2314fc6a02c5c0d7417c82a0bfcdf6dab796885
cjmurphy93 <cjmurph93@gmail.com>
2021-05-27 00:22:11 -0400

    AudioCache working

--- android/app/src/main/java/co/appbrewery/xylophone/MainActivity.java
+++ android/app/src/main/java/co/appbrewery/xylophone/MainActivity.java
@@ -1,6 +1,6 @@
 package co.appbrewery.xylophone;
 
-+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends FlutterActivity {
 
